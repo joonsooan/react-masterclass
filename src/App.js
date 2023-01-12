c56ed64f6033b332629a1452a5ffee938563c5a6
joonsooan <joonsooan@gmail.com>
2023-01-12 13:49:28 +0900

    imported styled-components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
+import styled from "styled-components";
+
+const Father = styled.div`
+  display: flex;
+`;
+
+const BoxOne = styled.div`
+  background-color: cornflowerblue;
+  width: 100px;
+  height: 100px;
+`;
+
+const BoxTwo = styled.div`
+  background-color: tomato;
+  width: 100px;
+  height: 100px;
+`;
+
+const Text = styled.span`
+  color: white;
+`;
+
 function App() {
   return (
-    <div style={{ display: "flex" }}>
-      <div
-        style={{ backgroundColor: "cornflowerblue", width: 100, height: 100 }}
-      ></div>
-      <div style={{ backgroundColor: "tomato", width: 100, height: 100 }}></div>
-    </div>
+    <Father>
+      <BoxOne>
+        <Text>Hello</Text>
+      </BoxOne>
+      <BoxTwo></BoxTwo>
+    </Father>
   );
 }
 
